7829dd77ff09387b1c5f47ec5af58c1a39eceac2
b08303021 <75900106+b08303021@users.noreply.github.com>
2020-12-16 20:15:29 +0800

    Update 頁面2.py

--- 頁面2.py
+++ 頁面2.py
@@ -14,11 +14,12 @@ class Game(tk.Frame):
         f1 = tkFont.Font(size = 30, family = "Courier New")
         f2 = tkFont.Font(size = 25, family = "Courier New")
         f3 = tkFont.Font(size = 20, family = "Courier New")
+        f4 = tkFont.Font(size = 10, family = "Courier New")
         # 背景和圖片
         self.background = tk.Canvas(self, height = 600, width = 800, bg = 'white').pack()
         self.picture = tk.Canvas(self, height = 240, width = 240, bg = 'blue').place(x = 60, y = 60)
         # 經驗值
-        self.exp = tk.Label(self, text = "經驗：100", height = 1, width = 12, bg = 'white', font = f3).place(x = 445, y = 260)
+        self.exp = tk.Label(self, text = "經驗：10/100", height = 1, width = 12, bg = 'white', font = f3).place(x = 450, y = 290)
         # 首頁基礎數值
         self.level = tk.Button(self, text = "等級：12", height = 1, width = 15, font = f3, anchor='w').place(x = 60, y = 360)
         self.blood = tk.Button(self, text = "血量：231", height = 1, width = 15, font = f3, anchor='w').place(x = 60, y = 420)
@@ -28,7 +29,9 @@ class Game(tk.Frame):
         self.dodge = tk.Button(self, text = "閃避：34%", height = 1, width = 15, font =f3, anchor='w').place(x = 400, y = 480)
         # 角色名和自己的名稱
         self.charater_name = tk.Label(self, text = "桐人", height = 1, width = 10, bg = 'white', font =f1).place(x = 420 , y = 100)
-        self.name = tk. Label(self, text = "亞斯娜", height = 1, width = 10, bg = 'white', font =f2).place(x = 440 , y = 200)
+        self.name = tk. Label(self, text = "亞斯娜", height = 1, width = 10, bg = 'white', font =f2).place(x = 80 , y = 310)
+        self.word = '桐谷和人（桐ヶ谷きりがや 和人かずと，Kirigaya Kazuto），虛擬世界的用戶名為「桐人（キリト，Kirito）」，是日本小說家川原礫輕小說《刀劍神域》及其改編動、漫畫作品的主角。在以《刀劍神域》為主的諸多系列作裡作為主角或重要角色登場。'
+        self.intro = tk.Message(self, text = self.word, font = f4, bg = 'white', width = 270).place(x = 400, y = 150)
         # 按鈕
         self.boss = tk.Button(self, text = "關\n卡", height = 2, width = 2, font = f3).place(x = 760 , y = 60)
         self.character = tk.Button(self, text = "角\n色", height = 2, width = 2, font = f3).place(x = 760 , y = 180)
